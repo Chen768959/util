0c70e32a2a18a1f980f567fa2cb0e97a29b98e04
Chen768959 <934103231@qq.com>
2021-04-30 18:05:01 +0800

    重构，且增加字符串替换关键字和if关键字

--- sql_util/src/main/java/NamedSqlUtil.java
+++ sql_util/src/main/java/NamedSqlUtil.java
@@ -1,6 +1,8 @@
 import java.text.StringCharacterIterator;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class NamedSqlUtil {
     /**
@@ -110,7 +112,7 @@ public class NamedSqlUtil {
     private boolean checkTargetTagCur(StringCharacterIterator targetSqlIterator, char... targetChar){
         for (int i=0 ; i<targetChar.length; i++){
             if (targetSqlIterator.current() != targetChar[i]){
-                for (int j=i ; j<=0 ; j--){
+                for ( ; i > 0 ; i--){
                     targetSqlIterator.previous();
                 }
                 return false;
@@ -131,7 +133,7 @@ public class NamedSqlUtil {
      * @return void
      */
     private void analyseReplaceLogic(StringCharacterIterator targetSqlIterator, Map<String, Object> paramMap, StringBuilder resSql) {
-        String strKey = traveToSpace(targetSqlIterator);
+        String strKey = traveToSpecial(targetSqlIterator);
 
         Object resValue = paramMap.get(strKey);
         if (resValue == null){
@@ -152,7 +154,7 @@ public class NamedSqlUtil {
      * @return void
      */
     private void analysePlaceholderLogic(StringCharacterIterator targetSqlIterator, Map<String, Object> paramMap, StringBuilder resSql, List<Object> resPrmList) {
-        String strKey = traveToSpace(targetSqlIterator);
+        String strKey = traveToSpecial(targetSqlIterator);
 
         Object resValue = paramMap.get(strKey);
         if (resValue == null){
@@ -177,7 +179,7 @@ public class NamedSqlUtil {
         int loopNum = 0;
 
         // 判断if的strKey是否存在
-        String strKey = traveToSpace(targetSqlIterator);
+        String strKey = traveToSpecial(targetSqlIterator);
         Object resValue = paramMap.get(strKey);
 
         // 当前指针指向if的strKey的后一位
@@ -190,7 +192,7 @@ public class NamedSqlUtil {
                     loopNum++;
                 }else if (c == '}'){
                     if (loopNum <= 0){
-                        targetSqlIterator.next();//跳出if，且将下标移向if后一位
+                        c = targetSqlIterator.next();//跳出if，且将下标移向if后一位
                         break;
                     }else {
                         loopNum--;
@@ -213,7 +215,7 @@ public class NamedSqlUtil {
                 loopNum++;
             }else if (c == '}'){
                 if (loopNum <= 0){
-                    targetSqlIterator.next();//跳出if，且将下标移向if后一位
+                    c = targetSqlIterator.next();//跳出if，且将下标移向if后一位
                     break;
                 }else {
                     loopNum--;
@@ -236,7 +238,67 @@ public class NamedSqlUtil {
      * @return void
      */
     private void analyseListLogic(StringCharacterIterator targetSqlIterator, Map<String, Object> paramMap, StringBuilder resSql, List<Object> resPrmList) {
+        char c = targetSqlIterator.current();
+        StringBuilder listKey = new StringBuilder(); // listKey
+        StringBuilder intervalStr = new StringBuilder(); // 间隔符
+        StringBuilder loopStrBuilder = new StringBuilder(); // 循环体原生内容
 
+        // 解析listKey
+        while ( c != ':' && c != StringCharacterIterator.DONE){
+            listKey.append(c);
+            c = targetSqlIterator.next();
+        }
+        List<Map<String, Object>> loopParamList = (List<Map<String, Object>>) paramMap.get(listKey.toString());
+        if (loopParamList == null){
+            // logger.warn("analyseListLogic失败，key不存在："+listKey);
+        }
+
+        // 解析间隔符
+        while ((! Character.isSpaceChar(c = targetSqlIterator.next())) && c != StringCharacterIterator.DONE){
+            intervalStr.append(c);
+        }
+
+        // 此时下标指向间隔符后一位空格处
+        // 解析循环体（此时保留原生内容）
+        int loopNum = 0;
+        while (c != StringCharacterIterator.DONE){
+            if (c == '{'){
+                loopNum++;
+            }else if (c == '}'){
+                if (loopNum <= 0){
+                    c = targetSqlIterator.next();//跳出list，且将下标移向list后一位
+                    break;
+                }else {
+                    loopNum--;
+                }
+            }
+
+            loopStrBuilder.append(c);
+            c = targetSqlIterator.next();
+        }
+        // 循环体解析完毕
+        String loopStr = loopStrBuilder.toString();
+        // 根据map数循环list
+        Optional.ofNullable(loopParamList).orElse(new ArrayList<>()).forEach(map -> {
+            // 每一次循环的参数集是当前map对象，作用的原始sql域则是此次的循环体，但是总的结果sql和结果填充list都没变
+            StringCharacterIterator loopIterator = new StringCharacterIterator(loopStr);
+            while (loopIterator.current() != StringCharacterIterator.DONE){
+                checkSpecialAndAct(loopIterator, map, resSql, resPrmList);
+
+                if (loopIterator.current() != StringCharacterIterator.DONE){
+                    resSql.append(loopIterator.current());
+                    loopIterator.next();
+                }
+            }
+
+            //结尾处加上间隔符
+            resSql.append(' ').append(intervalStr);
+        });
+
+        // 清除末尾间隔符
+        if ( ! Optional.ofNullable(loopParamList).orElse(new ArrayList<>()).isEmpty()){
+            resSql.delete(resSql.length() - intervalStr.length(), resSql.length());
+        }
     }
 
     /**
@@ -245,12 +307,12 @@ public class NamedSqlUtil {
      * @author Chen768959
      * @return java.lang.String
      */
-    private String traveToSpace(StringCharacterIterator targetSqlIterator){
+    private String traveToSpecial(StringCharacterIterator targetSqlIterator){
         char c = targetSqlIterator.current();
         StringBuilder strKey = new StringBuilder();
 
         //判断if的strKey是否存在，不存在则忽略if内容
-        while ( Character.isLowerCase(c) || Character.isUpperCase(c) || Character.isDigit(c) ){
+        while ( (Character.isLowerCase(c) || Character.isUpperCase(c) || Character.isDigit(c)) && c != StringCharacterIterator.DONE ){
             strKey.append(c);
             c = targetSqlIterator.next();
         }
